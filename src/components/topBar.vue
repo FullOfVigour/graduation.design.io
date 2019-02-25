b4c985c82c60ce1d3180ae02c9aa5961442a7fed
xuhan <xuhan@spacetech.com.cn>
2019-02-25 18:02:16 +0800

    添加 topbar组件(未完成)

---
+++ src/components/topBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="top__bar">
+    <div class="top__bar-logo">
+      <img class="logo__img"
+        src="../assets/logo.png">
+      <span class="logo__span">综合实践管理平台</span>
+    </div>
+    <div class="top__bar-information">
+      <span class="information__time">{{time}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  computed: {
+    time() {
+      return moment(1549555199999).format('YYYY年MMMD日dddd')
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.top__bar {
+  width: 100%;
+  padding: 5px 10px;
+  background-color: #fff;
+  &-logo {
+    height: 64px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+  .logo {
+    &__img {
+      height: 60px;
+    }
+    &__span {
+      color: #2c5590;
+      font-size: 36px;
+      font-weight: 600;
+    }
+  }
+
+  &-information {
+  }
+  .information {
+    &__time {
+      font-size: 14px;
+    }
+  }
+}
+</style>
+
