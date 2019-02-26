338dbff2e7e8c8a13ef62c79feb6df62ac345d44
xuhan <xuhan@spacetech.com.cn>
2019-02-26 18:07:44 +0800

    no message

--- src/modules/main/index.vue
+++ src/modules/main/index.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <top-bar></top-bar>
+    <!-- <top-bar></top-bar> -->
+    <div style="margin:20px;">
+      <calendar></calendar>
+    </div>
+    <testa></testa>
   </div>
 </template>
 
 <script>
 import topBar from '../../components/topBar'
+import calendar from '../../components/calendar'
+import testa from './a'
+import { mapActions } from 'vuex'
+import { getBaseData } from '../../storage/localStorage'
 export default {
   components: {
-    topBar
+    topBar,
+    calendar,
+    testa
+  },
+  created() {
+    this.initApp(getBaseData()).then(() => {})
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    ...mapActions(['initApp'])
   }
 }
 </script>
