1518b1e94d195f56a61fa2ae78c5b105b31a525d
xuhan <xuhan@spacetech.com.cn>
2019-04-02 10:52:53 +0800

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,35 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
   env: {
-    browser: true
+    node: true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: ['html'],
-  // add your custom rules here
+  plugins: ['vue'],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
   rules: {
-    // allow paren-less arrow functions
+    'prettier/prettier': [
+      'off',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        parser: 'flow'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'arrow-parens': 0,
-    // allow async-await
     'generator-star-spacing': 0,
     'space-before-function-paren': 0,
     'standard/computed-property-even-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-unused-expressions': 0
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
   }
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 import { SET_BASE_DATA } from './mutation-types'
 
 export default {
+  // eslint-disable-next-line no-unused-vars
   initApp({ commit, dispatch, state }, baseData) {
     commit(SET_BASE_DATA, baseData)
     // document.title = baseData.companyName
